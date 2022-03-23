c4562d117dd7a5cfae3e14322bea2d7710ff6e79
Anders Moldin <anders.moldin@lakemedelsverket.se>
2022-03-23 08:59:27 +0100

    Initial commit

--- Molledikrysset/Molledikrysset.Designer.cs
+++ Molledikrysset/Molledikrysset.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Molledikrysset
 {
-    partial class Form1
+    partial class Molledikrysset
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Molledikrysset
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1228, 885);
+            this.Name = "Molledikrysset";
+            this.Text = "Molledikrysset";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Molledikrysset/Molledikrysset.cs
+++ Molledikrysset/Molledikrysset.cs
@@ -1,8 +1,8 @@
 namespace Molledikrysset
 {
-    public partial class Form1 : Form
+    public partial class Molledikrysset : Form
     {
-        public Form1()
+        public Molledikrysset()
         {
             InitializeComponent();
         }
